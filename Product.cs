c6d29d02fc3ecdb24d600b45192fcdc63d7c164a
Mojito <vu3xk41997@gmail.com>
2023-09-02 22:48:50 +0200

    add files

---
+++ Product.cs
@@ -0,0 +1,76 @@
+﻿using System;
+namespace ShoppingBasket
+{
+	internal class Product
+	{
+		//input variable
+		private string name;
+		private double price;
+		private int quantity;
+
+		//output variable
+		public double amountToPay;
+
+		//method
+		public void Start()
+		{
+			ReadInput();
+			CalculateAmountToPay();
+			PrintReceipt();
+		}
+
+		private void ReadInput()
+		{
+			ReadName();
+			ReadPrice();
+			ReadQuantity();
+		}
+		
+		private void ReadName()
+		{
+			Console.WriteLine("Name of the product?");
+			name = Console.ReadLine();
+			Console.WriteLine();
+			Console.WriteLine("Thanks " + name + "!");
+		}
+
+		private void ReadPrice()
+		{
+			Console.WriteLine("Price:");
+			string strPrice = Console.ReadLine();
+            price = double.Parse(strPrice);
+            Console.WriteLine();
+            Console.WriteLine("The price of " + name + " is " + price + ".");
+        }
+
+		private void ReadQuantity()
+		{
+			Console.WriteLine("Quantity of the product?");
+			string strQuantity = Console.ReadLine();
+			quantity = int.Parse(strQuantity);
+			Console.WriteLine();
+			Console.WriteLine("The number of this product is " + quantity + ".");
+		}
+
+		private void CalculateAmountToPay()
+		{
+			amountToPay = price * quantity;
+		}
+
+		private void PrintReceipt()
+		{
+			Console.WriteLine("***** Receipt *****");
+			Console.WriteLine();
+
+			Console.WriteLine("Product: " + name);
+            Console.WriteLine("Price: " + price);
+            Console.WriteLine("Quantity: " + quantity);
+            Console.WriteLine();
+
+			Console.WriteLine("Amount to pay: " + amountToPay.ToString("0.00"));
+			Console.WriteLine();
+			Console.WriteLine("***** Welcome back *****");
+        }
+	}
+}
+
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ShoppingBasket;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to my shop!");
+        Product product = new Product();
+        product.Start();
+    }
+}
+
